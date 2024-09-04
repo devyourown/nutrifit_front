22294f23f5b59b0676014431ae685efcbdd93b16
devyourown <hyojoon56@gmail.com>
2024-09-04 17:52:05 +0900

    chore : use redis pw

--- app/lib/cache/data.ts
+++ app/lib/cache/data.ts
@@ -6,58 +6,47 @@ const redisClient = createClient({
         host: process.env.REDIS_HOST,
         port: Number(process.env.REDIS_PORT),
     },
+    password: process.env.REDIS_PW,
 });
 
 redisClient.on("error", (err: any) => console.error(err));
 
 if (!redisClient.isOpen) redisClient.connect();
 
-
 export async function saveCart(id: string, cart: Cart) {
-    await redisClient.hSet(
-        "cart",
-        id,
-        JSON.stringify(cart));
+    await redisClient.hSet("cart", id, JSON.stringify(cart));
 }
 
 export async function getCart(id: string) {
     try {
-        const cart = await redisClient.hGet('cart',
-            id
-        );
+        const cart = await redisClient.hGet("cart", id);
         if (cart === null) return null;
         return JSON.parse(cart!);
     } catch (error) {
-        console.error('Getting cart error: ', error);
+        console.error("Getting cart error: ", error);
         return null;
     }
 }
 
 export async function clearCacheCart(id: string, items: CartItem[]) {
     try {
-        await redisClient.hDel('cart', id);
+        await redisClient.hDel("cart", id);
     } catch (error) {
-        console.error('failed to clear cart.');
+        console.error("failed to clear cart.");
     }
 }
 
 export async function saveCheckout(id: string, checkout: Checkout) {
-    await redisClient.hSet(
-        "checkout",
-        id,
-        JSON.stringify(checkout)
-    );
+    await redisClient.hSet("checkout", id, JSON.stringify(checkout));
 }
 
 export async function getCheckout(id: string) {
     try {
-        const checkout = await redisClient.hGet('checkout',
-            id
-        );
+        const checkout = await redisClient.hGet("checkout", id);
         if (checkout === null) return null;
         return JSON.parse(checkout!);
     } catch (error) {
-        console.error('Getting checkout error: ', error);
+        console.error("Getting checkout error: ", error);
         return null;
     }
 }
