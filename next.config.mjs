64fff39ba30df96dc8a98a63a561214c015e54bf
devyourown <hyojoon56@gmail.com>
2024-09-06 17:54:10 +0900

    feat : add cross origin opener policy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                // 특정 경로에만 Cross-Origin-Opener-Policy를 설정
+                source: "/lib/auth", // 팝업을 여는 경로 또는 부모 페이지의 경로
+                headers: [
+                    {
+                        key: "Cross-Origin-Opener-Policy",
+                        value: "same-origin",
+                    },
+                    {
+                        key: "Cross-Origin-Embedder-Policy",
+                        value: "require-corp",
+                    },
+                ],
+            },
+        ];
+    },
+};
 
 export default nextConfig;
