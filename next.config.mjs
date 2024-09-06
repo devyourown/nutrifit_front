4e016c040ce8b10fa65198c6d758515d6bc7f360
devyourown <hyojoon56@gmail.com>
2024-09-06 18:01:32 +0900

    fix : fix crossorigin

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,20 @@ const nextConfig = {
                     },
                 ],
             },
+            {
+                // 특정 경로에만 Cross-Origin-Opener-Policy를 설정
+                source: "/auth/callback/:path*", // 팝업을 여는 경로 또는 부모 페이지의 경로
+                headers: [
+                    {
+                        key: "Cross-Origin-Opener-Policy",
+                        value: "same-origin",
+                    },
+                    {
+                        key: "Cross-Origin-Embedder-Policy",
+                        value: "require-corp",
+                    },
+                ],
+            },
         ];
     },
 };
